d8ee772f3c0c6998a8cd1b821d8fe5e6154590b0
chwang9 <chwang9@iflytek.com>
2021-02-05 10:10:48 +0800

    测试

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,3 +1,40 @@
 <template>
-    <div>1111</div>
+  <div class="Echarts">
+    <div id="main" style="width: 600px;height:400px;"></div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Echarts',
+  methods:{
+	  myEcharts(){
+		  // 基于准备好的dom，初始化echarts实例
+		  var myChart = this.$echarts.init(document.getElementById('main'));
+		  // 指定图表的配置项和数据
+		  var option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar',
+            }]
+		  };
+		  // 使用刚指定的配置项和数据显示图表。
+		  myChart.setOption(option);
+	  }
+  },
+  mounted() {
+  	this.myEcharts();
+  }
+}
+</script>
+
+<style>
+
+</style>
